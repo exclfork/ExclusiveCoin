a4e0daa6cc6121e04c5d50f8a68c6fbf0212a890
sunerok <justinvforvendetta@users.noreply.github.com>
2017-07-18 21:04:01 -0400

    fix freeze

--- src/qt/masternodemanager.h
+++ src/qt/masternodemanager.h
@@ -6,6 +6,10 @@
 
 #include <QWidget>
 #include <QTimer>
+#include <QFuture>
+
+#define MASTERNODELIST_UPDATE_SECONDS 15
+#define MASTERNODELIST_FILTER_COOLDOWN_SECONDS 3
 
 namespace Ui {
     class MasternodeManager;
@@ -29,6 +33,8 @@ public:
 
     void setClientModel(ClientModel *clientModel);
     void setWalletModel(WalletModel *walletModel);
+    
+    void updateListConc();
 
 
 public slots:
@@ -44,6 +50,9 @@ private:
     ClientModel *clientModel;
     WalletModel *walletModel;
     CCriticalSection cs_adrenaline;
+    int64_t nTimeFilterUpdated;
+	bool fFilterUpdated;
+	QFuture<void> f1;
 
 private slots:
     void on_createButton_clicked();
